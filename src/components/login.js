69a0b672f774641ab216252bb3a0256532dd992f
ya1k96 <yamil123_1@live.com>
2021-06-21 00:58:27 -0300

    update

--- src/components/login.js
+++ src/components/login.js
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import logo from '../images/eRent144x144.png';
+import ReactDOM from 'react-dom';
 
 let initialState = {
     email: {
@@ -12,29 +13,35 @@ let initialState = {
         msg: '',
         value: ''
     },
+    loading: false,
     className: ` h-10 
     focus:rounded-full focus:ring
     focus:outline-none block w-full pl-7 pr-12 md:text-sm sm:text-lg border-gray-300 rounded-full border-2 `
 };    
-let getValueForm = (event, setState) => {
-    console.log(setState)
-    // this.setState({
-    //     [event.target.name]: {value: event.target.value}
-    // });
+let getValueForm = (event, state, setState) => {
+    state[event.target.name].value = event.target.value;
+    setState(state);
 }
 
-let resetFieldClass = (event) => {
-    console.log(event)
-    // this.setState({
-    //     [event.target.name]: {msg: '', invalid: false}
-    // });
+let resetField = (state, setState) => {
+    state.email.invalid = false;
+    state.password.invalid = false;
+    state.email.msg = '';
+    state.password.msg = '';
+    
+    setState(state);
+
+    //Renderizamos los cambios
+    render({
+        email: state.email.value,
+        password: state.password.value
+    })     
 }
 
-let sendLogin = () => {
-
+let sendLogin = (state, setState) => {
     const body = {
-        email: this.state.email.value,
-        password: this.state.password.value
+        email: state.email.value,
+        password: state.password.value
     };
     fetch('http://192.168.1.8:8080/api/login',
     {
@@ -50,13 +57,24 @@ let sendLogin = () => {
             //prueba
         }
         resp.errors?.forEach(error => {
-            this.setState({
-                [error.param]: {invalid: true, msg: error.msg}
-            });
+            state[error.param].invalid = true;
+            state[error.param].msg = error.msg;
+
+            setState(state); 
+            //Renderizamos los cambios
+            render({
+                email: state.email.value,
+                password: state.password.value
+            })           
         });            
     });
 }
-export const Login = () => {
+
+let render = (props) => {
+    ReactDOM.render(<Login email={props.email} password={props.password}></Login>, document.getElementById('root'))
+}
+
+export const Login = (props = {email: '', password: ''} ) => {
     const [state, setState] = useState(initialState);
 
         return (<>
@@ -69,29 +87,41 @@ export const Login = () => {
                     <p className="text-gray-500 text-center mb-4 md:text-3xl sm:text-4xl">Ingresar</p>
                     <form action="">
                     <div className="mt-1 rounded-full relative shadow-sm">
-                        <input type="text" name="email" 
+                        <input type="text" name="email"
+                        onClick={()=>{resetField(state, setState)}} 
+                        defaultValue={props.email}
                         className={state.email.invalid
                         ? `border-red-400 ${state.className}` : state.className} 
-                        placeholder="Correo" onChange={getValueForm}
-                        onClick={resetFieldClass}>                            
+                        placeholder="Correo" onChange={
+                            (event) => {
+                                getValueForm(event, state, setState);
+                            }
+                        }
+                        >                            
                         </input>
-                        <p className="font-medium text-sm text-red-400">{state.email.msg}</p>
+                        <p className="font-medium text-sm text-red-400 animate__animated animate__fadeIn">{state.email.msg}</p>
                     </div>            
                         <div className="mt-1 rounded-full relative shadow-sm">
                             <input type="password" 
                             name="password"
+                            onClick={()=>{resetField(state, setState)}}
+                            defaultValue={props.password}
                             className={ state.password.invalid
                             ? `border-red-400  ${state.className}` : state.className} 
-                            placeholder="Contraseña" onChange={getValueForm}
-                            onClick={resetFieldClass}></input>   
-                            <p className="font-medium text-sm text-red-400">{state.password.msg}</p> 
+                            placeholder="Contraseña" onChange={
+                                (event) => {
+                                    getValueForm(event, state, setState);
+                                }
+                            }
+                            ></input>   
+                            <p className="font-medium text-sm text-red-400 animate__animated animate__fadeIn">{state.password.msg}</p> 
                         </div>    
                     </form>
 
                     <div className="grid grid-cols-1 mx-auto gap-y-5 mt-5">
                         <div className="rounded-full w-auto mx-auto">
-                            <a  className="px-8 py-1 text-base font-medium rounded-full text-white bg-blue-300  hover:text-blue-100 md:py-2 sm:py-3 sm:text-lg md:text-lg md:px-10 cursor-pointer focus:ring"
-                            onClick={sendLogin}>
+                            <a  className="px-8 py-1 text-base font-medium rounded-full text-white bg-blue-400  hover:text-blue-100 md:py-2 sm:py-3 sm:text-lg md:text-lg md:px-10 cursor-pointer"
+                            onClick={() => sendLogin(state, setState)}>                             
                                 Ingresar
                             </a>
                         </div>
