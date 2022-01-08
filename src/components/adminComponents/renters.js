2d5f088f38a6a97d6df69da27f4054545eac9ff7
ya1k96 <yamil123_1@live.com>
2022-01-07 22:21:58 -0300

    DEV-11: clases reutilizadas renters

--- src/components/adminComponents/renters.js
+++ src/components/adminComponents/renters.js
@@ -15,7 +15,7 @@ const Renters = () => {
     }, []);    
 
     return(<>
-    <div className="flex justify-center mt-5">
+    <div className="content-center mt-5">
         <Link to="/newrenter">
             <button className="btn-primary">
                 <p className="text-white font-medium">Nuevo inquilino</p>
@@ -24,19 +24,19 @@ const Renters = () => {
         {/* <Link to="/newrenter">
         </Link> */}
     </div>
-    <div className="flex justify-center mt-6">
+    <div className="content-center mt-5">
         {
         isLoading ? spinner() : 
-        <div className="grid md:grid-cols-3 sm:grid-cols-2 gap-4">
+        <div className="renters-grid">
             {                 
                 (array.length > 0 && !isLoading) ?
                 array.map(renter => <Link to={'/dashboard/renter/'+renter._id} key={renter._id}>
-                <div className="bg-white shadow-lg rounded-xl w-40 h-40 hover:bg-blue-200  cursor-pointer animate__animated animate__fadeIn">                        
+                <div className="renter-card">                        
                 <img className="m-auto mt-3 w-28 h-28" src={neutralUser} alt="invoice-logo"></img>
-                <p className="text-center font-medium text-gray-600 ">{renter.name +' '+ renter.surname}</p>
+                <p className="text-card">{renter.name +' '+ renter.surname}</p>
                 {
-                    renter.aldia ? <div className="flex justify-center">
-                        <p className="bg-green-400 rounded-full text-white text-sm font-sans w-1/3 pl-2 pr-1">Al dia</p>
+                    renter.aldia ? <div className="content-center">
+                        <p className="badge-green">Al dia</p>
                     </div> : ''
                 }
             </div></Link> ) : ''          
