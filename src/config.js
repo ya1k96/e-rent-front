cc704ef2d2480ef02dc613c37164e3f76a02bd66
ya1k96 <yamil123_1@live.com>
2022-01-09 17:21:36 -0300

    fixed url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export const config = {
-    URL_SERVER: "http://127.0.0.1:8080/api",
+    URL_SERVER: `${window.location.protocol}//${window.location.hostname}:${window.location.port}`,
     // URL_SERVER: "http://192.168.1.8:8080/api",
 }
+
